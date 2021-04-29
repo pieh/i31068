7784e158fbea64348ba77a9d0579f5d27d68bc40
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-04-29 16:53:08 +0200

    add valid avif

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,7 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
+    <img src="/test.avif" />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
